ad241e0696d55ef574b5f7966ea47d983edea56a
Shu Sean <shuseanlimy@gmail.com>
2024-12-18 17:00:59 +0800

    homev3

--- app/src/main/java/com/example/realestate/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/realestate/fragments/HomeFragment.kt
@@ -7,11 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.realestate.Navigation
+import com.example.realestate.R
 import com.example.realestate.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    private val itemDetailsFragment = ItemDetailsFragment()
     private val addFragment = AddFragment()
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -38,6 +40,16 @@ class HomeFragment : Fragment() {
             binding.snacksBtn
 
         )
+        val fruitItems = listOf(
+            binding.appleCard,
+            binding.mangoCard,
+            binding.watermelonCard,
+            binding.strawberryCard,
+            binding.bananaCard
+        )
+
+
+
 
         categoryBtns.forEach { category ->
             category.setOnClickListener {
@@ -52,6 +64,29 @@ class HomeFragment : Fragment() {
                 Toast.makeText(requireContext(), "Navigating to $categoryText category ", Toast.LENGTH_SHORT).show()
             }
         }
+        fruitItems.forEach { itemCardView ->
+            itemCardView.setOnClickListener{
+                val mainActivity = requireActivity() as? Navigation
+
+                // Get the content description of the ImageButton (assuming it holds the category text)
+                val categoryText = itemCardView.contentDescription.toString()
+                // Call the showAddFragment() method on the MainActivity
+                mainActivity?.showAddFragment(categoryText)
+
+                Toast.makeText(requireContext(), "Navigating to $categoryText category ", Toast.LENGTH_SHORT).show()
+                val bundle = Bundle()
+                bundle.putString("item", categoryText) // Pass item data so that ItemDetails know which item to show
+                itemDetailsFragment.arguments = bundle
+
+                // Perform the fragment transaction
+                val transaction = requireActivity().supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.rootFl, itemDetailsFragment)
+//                    transaction.addToBackStack(null)
+                transaction.commit()
+                Toast.makeText(requireContext(),"${itemCardView.contentDescription} details shown",Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
 
